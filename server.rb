8ef9730380d0a745f38fe4b31a0364570937ea4c
Kenneth Vestergaard <kvs@binarysolutions.dk>
2011-11-21 19:03:06 +0100

    replace Node.js-based backend with a Sinatra-based, thus nuking JavaScript on the server entirely.

---
+++ server.rb
@@ -0,0 +1,32 @@
+require 'bundler/setup'
+require 'sinatra'
+require 'json'
+
+set :public_folder, File.dirname(__FILE__) + '/public'
+
+# Never return any documents - handled by webserver returning static files
+get '/documents/*' do
+  halt 404
+end
+
+# Default GET request for new pages.
+get '*' do
+  content_type :html
+  File.read 'public/index.html'
+end
+
+# Default POST request - attempts to store the document under 'public/documents/'
+post '/documents/*.json' do
+  doc = Pathname('public/documents') + Pathname(params[:splat].first + '.md').cleanpath
+  exists = doc.exist?
+
+  doc.dirname.mkpath
+  doc.open('w') { |f| f.write params[:text] }
+
+  message = (exists ? "Page updated." : "Page created.");
+
+  puts doc.to_s
+    
+  content_type :json
+  { status: "success", message: message}.to_json.to_s
+end
